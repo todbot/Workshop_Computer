3c1a5acd8992f5387353a537159e1a9545a1c0a2
Dune Desormeaux <dune.desormeaux@gmail.com>
2025-03-20 12:48:51 -0700

    separate clocks and fix LED rendering

--- releases/04_Weirding_Machine/main.cpp
+++ releases/04_Weirding_Machine/main.cpp
@@ -20,8 +20,8 @@ public:
 		// CONSTRUCTOR
 		for (int i = 0; i < SHIFT_REG_SIZE; i++)
 		{
-			bits[i] = false;
-			xorBits[i] = false;
+			bits[i] = true;
+			xorBits[i] = true;
 		}
 	}
 
@@ -29,11 +29,31 @@ public:
 	{
 		int16_t runglerOut = 0;
 		int16_t xorOut = 0;
-		
-		clockTick = PulseIn1RisingEdge();
+
+		mainClock = PulseIn1RisingEdge();
+
+		if (Connected(Input::Pulse2))
+		{
+			xorClock = PulseIn2RisingEdge();
+		}
+		else
+		{
+			xorClock = mainClock;
+		}
+
 		mainKnob = KnobVal(Main);
 
-		if (clockTick)
+		if (Connected(Input::Audio2))
+		{
+			vca = AudioIn2() + 2048;	  // Convert to 0-4095
+			vca = vca * KnobVal(Y) >> 12; // Attenuate with Y knob
+		}
+		else
+		{
+			vca = KnobVal(Y);
+		}
+
+		if (mainClock || xorClock)
 		{
 			if (Connected(Input::Audio1))
 			{
@@ -43,23 +63,38 @@ public:
 			{
 				data = rnd12();
 			}
-			// Shift the bits
-			rotateBits();
-			bool comparator = data > mainKnob;
+
+			comparator = data > mainKnob;
+		}
+
+		if (mainClock)
+		{
+			// Shift the main bits
+			rotate(bits);
 
 			if (comparator)
 			{
 				bits[0] = !bits[0]; // Toggle the first bit
+			}
+		}
 
-				if (!xorBits[SHIFT_REG_SIZE - 1])
-				{
-					xorBits[0] = !xorBits[0]; // Toggle the first xor bit
-				}
-			} else
+		if (xorClock)
+		{
+			// Shift the xor bits
+			rotate(xorBits);
+
+			if (comparator)
 			{
-				if (xorBits[SHIFT_REG_SIZE - 1])
+				if (!bits[SHIFT_REG_SIZE - 1])
 				{
-					xorBits[0] = !xorBits[0]; // Toggle the first xor bit
+					xorBits[0] = !xorBits[0]; // Toggle the first bit
+				}
+			}
+			else
+			{
+				if (bits[SHIFT_REG_SIZE - 1])
+				{
+					xorBits[0] = !xorBits[0]; // Toggle the first bit
 				}
 			}
 		}
@@ -70,50 +105,57 @@ public:
 			xorOut |= (xorBits[i] << (i - (SHIFT_REG_SIZE - RUNGLER_DAC_BITS)));
 		}
 
-		//convert 3 bit outputs to 12 bit values between -2048 and 2047
+		// convert 3 bit outputs to 12 bit values between -2048 and 2047
 		runglerOut = (runglerOut << (12 - RUNGLER_DAC_BITS));
 		xorOut = (xorOut << (12 - RUNGLER_DAC_BITS));
 
-		//Attenuate the outputs based on the y knob
-		runglerOut = (runglerOut * KnobVal(Y)) >> 12;
-		xorOut = (xorOut * KnobVal(Y)) >> 12;
+		// Attenuate the outputs based on the vca value
+		runglerOut = (runglerOut * vca) >> 12;
+		xorOut = (xorOut * vca) >> 12;
 
 		runglerOut -= 2048; // Convert to -2048 to 2047
-		xorOut -= 2048; // Convert to -2048 to 2047
-	
+		xorOut -= 2048;		// Convert to -2048 to 2047
 
-		//Output rungler signals
+		// Output rungler signals
 		AudioOut1(runglerOut);
 		AudioOut2(xorOut);
+
+		//Output CV signals
+
+		//Output pulse signals
+		PulseOut1(bits[SHIFT_REG_SIZE - 1]);
+		PulseOut2(xorBits[SHIFT_REG_SIZE - 1]);
 
 		// show shiftreg state on LEDs
 		for (int i = 0; i < 6; i++)
 		{
-			LedOn(i, bits[i]);
+			bool *selectBits = (Connected(Input::Pulse2) && !Connected(Input::Pulse1)) ? xorBits : bits;
+			LedBrightness(ledMap[i], (selectBits[i] ? KnobVal(Y) : 0) * 4095 >> 12);
 		}
 	}
 
 private:
 	bool bits[SHIFT_REG_SIZE];
 	bool xorBits[SHIFT_REG_SIZE];
-	bool clockTick = false;
+	bool mainClock = false;
+	bool xorClock = false;
 	int16_t mainKnob;
 	int16_t data;
+	int8_t ledMap[6] = {0, 2, 4, 1, 3, 5};
+	int16_t vca = 0;
+	bool comparator;
 
-	void rotateBits()
+	void rotate(bool *array)
 	{
 		// Shift the bits to the right
-		bool lastBit = bits[SHIFT_REG_SIZE - 1];
-		bool lastXorBit = xorBits[SHIFT_REG_SIZE - 1];
+		bool lastBit = array[SHIFT_REG_SIZE - 1];
 
 		for (int i = SHIFT_REG_SIZE - 1; i > 0; i--)
 		{
-			bits[i] = bits[i - 1];
-			xorBits[i] = xorBits[i - 1];
+			array[i] = array[i - 1];
 		}
 		// Set the first bit to the new value
-		bits[0] = lastBit;
-		xorBits[0] = lastXorBit;
+		array[0] = lastBit;
 	}
 };
 
