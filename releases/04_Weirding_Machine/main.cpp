59efa439e7dfe47c6dbee8fc09120bc6e741c1ec
Dune Desormeaux <dune.desormeaux@gmail.com>
2025-03-19 17:31:15 -0700

    Noise out of Audio1

--- releases/04_Weirding_Machine/main.cpp
+++ releases/04_Weirding_Machine/main.cpp
@@ -1,42 +1,50 @@
 #include "ComputerCard.h"
 
-/// Passthrough
-/// Connects audio, CV and pulse inputs directly to corresponding outputs
-/// Also displays switch position and knob values on LEDs
-class Passthrough : public ComputerCard
+// 12 bit random number generator
+uint32_t __not_in_flash_func(rnd12)()
+{
+    static uint32_t lcg_seed = 1;
+    lcg_seed = 1664525 * lcg_seed + 1013904223;
+    return lcg_seed >> 20;
+}
+
+/// Weirding Machine
+class WeirdingMachine : public ComputerCard
 {
 public:
-	
+
+	WeirdingMachine()
+	{
+		//CONSTRUCTOR
+		// Set up the shift register
+		vShiftReg = 0;
+	}
+
 	virtual void ProcessSample()
 	{
-		// Transfer audio/CV/Pulse inputs directly to outputs
-		AudioOut1(AudioIn1());
-		AudioOut2(AudioIn2());
-
-		CVOut1(CVIn1());
-		CVOut2(CVIn2());
-
-		PulseOut1(PulseIn1());
-		PulseOut2(PulseIn2());
-
-		// Get switch position and set LEDs 0, 2, 4 accordingly
-		int s = SwitchVal();
-		LedOn(4, s == Switch::Down);
-		LedOn(2, s == Switch::Middle);
-		LedOn(0, s == Switch::Up);
-
-		// Set LED 1, 3, 5 brightness to knob values
-		LedBrightness(1, KnobVal(Knob::Main));
-		LedBrightness(3, KnobVal(Knob::X));
-		LedBrightness(5, KnobVal(Knob::Y));
+		AudioOut1(rnd12() - 2048);
 	}
+
+	void clip(int32_t &a)
+    {
+        if (a < -2047)
+            a = -2047;
+        if (a > 2047)
+            a = 2047;
+    };
+
+private:
+
+int8_t vShiftReg;
+
 };
 
 
 int main()
 {
-	Passthrough pt;
-	pt.Run();
+	WeirdingMachine wm;
+	wm.EnableNormalisationProbe();
+	wm.Run();
 }
 
   
